8c0f69a894d3e21143e56e2c7cf4ef32829e08ec
Remco Brosky <remcobrosky@microsoft.com>
2023-03-03 09:39:09 +0100

    refactoring

--- WorkloadIdentity.Web/Pages/Index.cshtml.cs
+++ WorkloadIdentity.Web/Pages/Index.cshtml.cs
@@ -2,16 +2,19 @@
 using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WorkloadIdentity.Web.Helpers;
 
 namespace WorkloadIdentity.Web.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IWebHostEnvironment _environment;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment environment)
         {
             _logger = logger;
+            _environment = environment;
         }
 
         public void OnGet()
@@ -24,19 +27,18 @@ namespace WorkloadIdentity.Web.Pages
             string keyvaultUrl = "https://rbr-kv-we.vault.azure.net/";
             string secretName = "supersecret";
 
-            DefaultAzureCredentialOptions options = new DefaultAzureCredentialOptions
-            {
-                ManagedIdentityClientId = "deb5d59e-8a1d-4860-8342-0eee384b3057"
+            
+            DefaultAzureCredentialOptions options = 
+                DefaultCredentialOptions.GetDefaultAzureCredentialOptions(
+                    "deb5d59e-8a1d-4860-8342-0eee384b3057", 
+                    _environment);
 
-            };
-
-            try
-            {
+            try {
                 SecretClient client = new SecretClient(
                   new Uri(keyvaultUrl),
                   new DefaultAzureCredential(options));
 
-                // <getsecret>
+                
                 var keyvaultSecret = client.GetSecret(secretName).Value;
                 ViewData["Secret"] = $"Your secret is {keyvaultSecret.Value}";
             }
